8e3fdaefebfa3d37759f6872350ef2e594b52ecc
RSLi <robertsli@163.com>
2017-01-25 10:56:35 -0500

    Implement Person2.java with Fisher–Yates shuffle

--- src/main/java/edu/gatech/oad/antlab/person/Person2.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person2.java
@@ -1,12 +1,15 @@
 package edu.gatech.oad.antlab.person;
 
+import java.util.Random;
+
 /**
  *  A simple class for person 2
  *  returns their name and a
- *  modified string 
+ *  modified string
  *
  * @author Bob
- * @version 1.1
+ * @author Siwei Li
+ * @version 1.2
  */
 public class Person2 {
     /** Holds the persons real name */
@@ -31,14 +34,29 @@ public class Person2 {
 	 */
 	private String calc(String input) {
 	  //Person 2 put your implementation here
-	  return null;
+	  String output = "";
+      Random rand = new Random();
+      String[] arr = input.split("");
+
+      for (int i = arr.length - 1; i > 0; i--) {
+          int index = rand.nextInt(i + 1);
+          String temp = arr[i];
+          arr[i] = arr[index];
+          arr[index] = temp;
+      }
+
+      for (int i = 0; i < arr.length; i++) {
+          output += arr[i];
+      }
+      
+	  return output;
 	}
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
