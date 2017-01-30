99ca19f7794d882d241eedc5dcb8d0cbddbba9b0
Theresa Ming <theresaming@lawn-128-61-65-124.lawn.gatech.edu>
2017-01-30 12:45:04 -0500

    fixed person 3 method

--- src/main/java/edu/gatech/oad/antlab/person/Person3.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person3.java
@@ -45,7 +45,7 @@ public class Person3 {
 	  //Person 3 put your implementation here
 	  String reversed = "";
 	 	for (int i = input.length() - 1; i >= 0; i--) {
-			reversed += i;
+			reversed += input.charAt(i);
 		}
 		return reversed;
 	}
