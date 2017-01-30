eb4d3c614e6c39fd8d252793da837f23c8aad141
nstoltzfus3 <nstoltzfus3@gatech.edu>
2017-01-30 18:51:22 -0500

    updated person1 code and updated input name in antLab.

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Nicholas Stoltzfus
  *  @version 1.1
  */
 public class Person1 {
@@ -31,7 +31,14 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+	  	StringBuffer s = new StringBuffer();
+		for (int i = 2; i < input.size(); i++) {
+			s.append(input.charAt(i));
+		}
+		s.append(input.charAt(0));
+		s.append(input.charAt(1));
+		input = s.toString();
+		return input;
 	}
 	
 	/**
